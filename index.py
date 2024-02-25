4ff656c529637292788a79399704852893d68808
zaenal_asy <48007545+zaenalalamsyah1@users.noreply.github.com>
2024-02-25 12:15:41 +0700

    nambahin file index

---
+++ index.py
@@ -0,0 +1,11 @@
+print("=== Aplikasi Biodata ====")
+
+name = "Udin"
+gender = input("masukan gender: ")
+tanggal_lahir = input("masukan tanggal lahir: ")
+hobi = input("masukan hobi: ")
+
+print("Nama : ", name)
+print("Gender: ", gender)
+print("tanggal_lahir: ", tanggal_lahir)
+print("hobi : ", hobi)
